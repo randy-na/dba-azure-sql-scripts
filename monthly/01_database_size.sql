e0b17d575c7919beb05dde5b81180029c41a29a0
Randy Nyambi Abine <Randy.NyambiAbine@landmark.co.uk>
2026-03-16 14:13:36 +0000

    update on database size query

--- monthly/01_database_size.sql
+++ monthly/01_database_size.sql
@@ -3,7 +3,13 @@
 -- Run on: Primary
 -- ============================================================
 SELECT
-    SUM(CASE WHEN type_desc = 'ROWS' THEN size END) * 8 / 1024.0  AS data_size_mb,
-    SUM(CASE WHEN type_desc = 'LOG'  THEN size END) * 8 / 1024.0  AS log_size_mb,
-    SUM(size) * 8 / 1024.0                                         AS total_size_mb
+    SUM(CASE WHEN type_desc = 'ROWS' THEN CAST(size AS BIGINT) END) * 8 / 1024 / 1024  AS data_size_Gb,
+    SUM(CASE WHEN type_desc = 'LOG'  THEN CAST(size AS BIGINT) END) * 8 / 1024 / 1024 AS log_size_Gb,
+    SUM(CAST(size AS BIGINT)) * 8 / 1024 / 1024                                        AS total_size_Gb
+FROM sys.database_files;
+
+SELECT
+    SUM(CASE WHEN type_desc = 'ROWS' THEN CAST(max_size AS BIGINT) END) * 8 / 1024 / 1024  AS max_data_size_Gb,
+    SUM(CASE WHEN type_desc = 'LOG'  THEN CAST(max_size AS BIGINT) END) * 8 / 1024 / 1024 AS max_log_size_Gb,
+    SUM(CAST(max_size AS BIGINT)) * 8 / 1024 / 1024                                        AS max_total_size_Gb
 FROM sys.database_files;
